da8b8dd5d415ae26be08bd70d3bca5b55e491b3b
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2020-09-28 10:40:32 -0300

    Primeira versao...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate gio;
+extern crate gtk;
+
+use std::env;
+
+use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
+use gtk::{
+    Application,
+    ApplicationWindow,
+    WidgetExt,
+    GtkWindowExt,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let application = Application::new(
+        "com.vilelapinheiro.jogos",
+        ApplicationFlags::empty())
+        .expect("Application initialization failed");
+
+    application.connect_startup(|app| {
+        let window = ApplicationWindow::new(&app);
+        window.set_title("Rusic");
+        window.show();
+    });
+
+    application.connect_activate(|_| {});
+    application.run(&env::args().collect::<Vec<_>>());
 }
