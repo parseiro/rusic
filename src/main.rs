10ca56b9810c8b292b78e41902c05f3479cd3c29
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2020-09-28 11:10:18 -0300

    Botoes iniciais

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ extern crate gtk;
 use std::env;
 
 use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
+use gtk::{Application, ApplicationWindow, GtkWindowExt,  WidgetExt};
 use gtk::{
-    Application,
-    ApplicationWindow,
-    WidgetExt,
-    GtkWindowExt,
+    ContainerExt,
+    SeparatorToolItem,
+    Toolbar,
+    ToolButton,
 };
+
+const PLAY_STOCK: &str = "gtk-media-play";
 
 fn main() {
     let application = Application::new(
@@ -17,10 +20,46 @@ fn main() {
         ApplicationFlags::empty())
         .expect("Application initialization failed");
 
-    application.connect_startup(|app| {
-        let window = ApplicationWindow::new(&app);
+    application.connect_startup(|application| {
+        let window = ApplicationWindow::new(&application);
         window.set_title("Rusic");
-        window.show();
+
+        let toolbar = Toolbar::new();
+        window.add(&toolbar);
+
+        {
+            let open_button = ToolButton::new_from_stock("gtk-open");
+            toolbar.add(&open_button);
+            toolbar.add(&SeparatorToolItem::new());
+
+            let previous_button = ToolButton::new_from_stock("gtk-media-previous");
+            toolbar.add(&previous_button);
+
+            let play_button = ToolButton::new_from_stock(PLAY_STOCK);
+            toolbar.add(&play_button);
+
+            let stop_button = ToolButton::new_from_stock("gtk-media-stop");
+            toolbar.add(&stop_button);
+
+            let next_button = ToolButton::new_from_stock("gtk-media-next");
+            toolbar.add(&next_button);
+
+            toolbar.add(&SeparatorToolItem::new());
+
+            let remove_button = ToolButton::new_from_stock("gtk-remove");
+            toolbar.add(&remove_button);
+
+            toolbar.add(&SeparatorToolItem::new());
+
+            let quit_button = ToolButton::new_from_stock("gtk-quit");
+            toolbar.add(&quit_button);
+        }
+
+
+
+
+
+        window.show_all();
     });
 
     application.connect_activate(|_| {});
