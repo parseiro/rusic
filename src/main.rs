359f0d26e24732f10186b914f7782f8740fda1cc
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2020-09-30 21:19:46 -0300

    Lints, melhorias...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+#![warn(anonymous_parameters)]
+// not useful anymore #![warn(box_pointers)]
+//#![warn(missing_docs)]
+#![warn(trivial_casts, trivial_numeric_casts)]
+#![warn(unused_results)]
+#![warn(unused_qualifications)]
+#![warn(variant_size_differences)]
+#![warn(clippy::cast_possible_truncation, clippy::cast_possible_wrap,
+clippy::cast_precision_loss, clippy::cast_sign_loss, clippy::integer_arithmetic)]
+#![warn(clippy::fallible_impl_from)]
+#![warn(clippy::filter_map, clippy::filter_map_next)]
+#![warn(clippy::if_not_else, clippy::nonminimal_bool, clippy::single_match_else)]
+#![warn(clippy::int_plus_one)]
+#![warn(clippy::similar_names)]
+#![warn(clippy::mutex_integer)]
+//#![warn(clippy::print_stdout,clippy::use_debug)]
+#![warn(clippy::unwrap_used, clippy::map_unwrap_or)]
+//#![warn(clippy::unwrap_in_result)]
+
+extern crate gdk_pixbuf;
 extern crate gio;
 extern crate gtk;
-extern crate gdk_pixbuf;
+extern crate gtk_sys;
 extern crate id3;
 
 use std::env;
+use std::rc::Rc;
 
 use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
 use gtk::{
@@ -22,9 +47,10 @@ use gtk::{
 };
 use gtk::Orientation::{Horizontal, Vertical};
 
+use crate::playlist::Playlist;
 use crate::toolbar::MusicToolbar;
 
-
+mod playlist;
 mod toolbar;
 
 struct App {
@@ -33,6 +59,7 @@ struct App {
     toolbar: MusicToolbar,
     window: ApplicationWindow,
     application: Application,
+    playlist: Rc<Playlist>,
 }
 
 impl App {
@@ -47,19 +74,24 @@ impl App {
         vbox.add(toolbar.toolbar());
 
         let cover = Image::new();
-        cover.set_from_file("cover.jpg");
+        // cover.set_from_file("cover.jpg");
         vbox.add(&cover);
+
+        let playlist = Rc::new(Playlist::new());
+        vbox.add(playlist.view());
 
         let adjustment = Adjustment::new(0.0, 0.0, 10.0, 0.0, 0.0, 0.0);
         let scale = Scale::new(Horizontal, &adjustment);
         scale.set_draw_value(false);
         vbox.add(&scale);
 
+
         window.show_all();
 
         let app = App {
             adjustment,
             cover,
+            playlist,
             toolbar,
             window,
             application,
@@ -78,11 +110,11 @@ fn main() {
         ApplicationFlags::empty())
         .expect("Application initialization failed");
 
-    application.connect_startup(|application| {
-        App::new(application.clone());
+    let _ = application.connect_startup(|application| {
+        let _ = App::new(application.clone());
     });
-    application.connect_activate(|_| {});
+    let _ = application.connect_activate(|_| {});
 
 
-    application.run(&env::args().collect::<Vec<_>>());
+    let _ = application.run(&env::args().collect::<Vec<_>>());
 }
--- src/toolbar.rs
+++ src/toolbar.rs
@@ -1,5 +1,11 @@
-use gtk::{ToolButton, Toolbar, SeparatorToolItem, ContainerExt, ToolButtonExt, WidgetExt};
+use std::path::PathBuf;
+
+use gtk::{ApplicationWindow, ContainerExt, DialogExt, FileChooserExt, FileFilterExt, Image, ImageExt, SeparatorToolItem, Toolbar, ToolButton, ToolButtonExt, WidgetExt};
+use gtk::{FileChooserAction, FileChooserDialog, FileFilter};
+use gtk_sys::{GTK_RESPONSE_ACCEPT, GTK_RESPONSE_CANCEL};
+
 use crate::App;
+use crate::playlist::Playlist;
 
 const PLAY_STOCK: &str = "gtk-media-play";
 const PAUSE_STOCK: &str = "gtk-media-pause";
@@ -14,6 +20,7 @@ pub struct MusicToolbar {
     stop_button: ToolButton,
     toolbar: Toolbar,
 }
+
 impl MusicToolbar {
     pub(crate) fn new() -> Self {
         let toolbar = Toolbar::new();
@@ -51,30 +58,80 @@ impl MusicToolbar {
             quit_button,
             remove_button,
             stop_button,
-            toolbar
+            toolbar,
         }
     }
 
     pub(crate) fn toolbar(&self) -> &Toolbar {
         &self.toolbar
     }
- }
+}
 
 impl App {
     pub fn connect_events(&self) {
         let window = self.window.clone();
 
-        self.toolbar.quit_button.connect_clicked(move |_| {
+        let _ = self.toolbar.quit_button.connect_clicked(move |_| {
             window.destroy();
         });
 
         let play_button = self.toolbar.play_button.clone();
-        self.toolbar.play_button.connect_clicked(move |_| {
+        let playlist = self.playlist.clone();
+        let cover = self.cover.clone();
+        let _ = self.toolbar.play_button.connect_clicked(move |_| {
             if play_button.get_stock_id() == Some(PLAY_STOCK.to_string()) {
                 play_button.set_stock_id(PAUSE_STOCK);
+                set_cover(&cover, &playlist);
             } else {
                 play_button.set_stock_id(PLAY_STOCK);
             }
         });
+
+        let parent = self.window.clone();
+        let playlist = self.playlist.clone();
+        let _ = self.toolbar.open_button.connect_clicked(move |_| {
+            let file = show_open_dialog(&parent);
+            if let Some(file) = file {
+                playlist.add(&file);
+            }
+        });
+
+        let playlist = self.playlist.clone();
+        let _ = self.toolbar.remove_button.connect_clicked(move |_| {
+            playlist.remove_selection();
+        });
     }
+}
+
+
+const RESPONSE_ACCEPT: i32 = GTK_RESPONSE_ACCEPT;
+const RESPONSE_CANCEL: i32 = GTK_RESPONSE_CANCEL;
+
+fn show_open_dialog(parent: &ApplicationWindow)
+                    -> Option<PathBuf> {
+    let mut file = None;
+    let dialog = FileChooserDialog::new(Some("Select an MP3 audio
+     file"),
+                                        Some(parent), FileChooserAction::Open);
+    {
+        let filter = FileFilter::new();
+        filter.add_mime_type("audio/mp3");
+        filter.set_name("MP3 audio file");
+        dialog.add_filter(&filter);
+    }
+
+    let _ = dialog.add_button("Cancel", RESPONSE_CANCEL);
+    let _ = dialog.add_button("Accept", RESPONSE_ACCEPT);
+
+    let result = dialog.run();
+    if result == RESPONSE_ACCEPT {
+        file = dialog.get_filename();
+    }
+    dialog.destroy();
+    file
+}
+
+fn set_cover(cover: &Image, playlist: &Playlist) {
+    cover.set_from_pixbuf(playlist.pixbuf().as_ref());
+    cover.show();
 }
